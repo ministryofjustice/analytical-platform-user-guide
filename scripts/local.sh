3e30d07f5ae722a18eb8e6e056062dae9f02ade5
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2024-02-22 17:32:11 +0000

    Fix whitespace in local.sh script
    Signed-off-by: Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>

--- scripts/local.sh
+++ scripts/local.sh
@@ -4,7 +4,7 @@ MODE="${1:-preview}"
 TECH_DOCS_PUBLISHER_IMAGE="docker.io/ministryofjustice/tech-docs-github-pages-publisher@sha256:cd3513beca3fcaf5dd34cbe81a33b3ff30337d8ada5869b40a6454c21d6f7684" # v4.0.0
 
 case ${MODE} in
-package | preview )
+package | preview)
   true
   ;;
 *)
